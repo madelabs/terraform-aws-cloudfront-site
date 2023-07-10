037e9560fdf0a69f3cd7575e97bb0e8b8f1715c1
Holden Porter <77367306+ml-holdenporter@users.noreply.github.com>
2023-07-10 14:22:51 -0400

    add origin shield arguments to example.tf

--- docs/example.tf
+++ docs/example.tf
@@ -3,8 +3,10 @@ module "s3_site" {
   source  = "madelabs/cloudfront-site/aws"
   version = "0.0.2"
 
-  index_document      = "index.html"
-  project_name        = "my-project"
-  domain_alias        = "my.project.com"
-  acm_certificate_arn = "arn:aws:acm:us-east-1:1234567890123:certificate/123abcde-f345-6789-0123-9abcdefgh01"
+  index_document        = "index.html"
+  project_name          = "my-project"
+  domain_alias          = "my.project.com"
+  acm_certificate_arn   = "arn:aws:acm:us-east-1:1234567890123:certificate/123abcde-f345-6789-0123-9abcdefgh01"
+  origin_shield_enabled = false
+  origin_shield_region  = "us-east-1"
 }
